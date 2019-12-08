205ff81f6e8c6109a200172f70b639a241a70c18
ProkurorNSK <proka@ngs.ru>
2019-12-08 20:03:48 +0700

    2.1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 import servlets.MirrorServlet;
+import servlets.SessionsServlet;
+import servlets.UsersServlet;
 
 /**
  * @author ProkurorNSK
@@ -13,13 +20,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
         MirrorServlet mirrorServlet = new MirrorServlet();
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/test");
         context.addServlet(new ServletHolder(mirrorServlet), "/mirror");
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
 
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
         server.start();
         System.out.println("Server started");
         server.join();
