6310944c4fd1d22f300383736f08c8aab163b7e3
ProkurorNSK <proka@ngs.ru>
2020-05-12 00:33:02 +0700

    7.2 Race

---
+++ Rase/src/main/java/CallCounter.java
@@ -0,0 +1,11 @@
+public class CallCounter {
+    private int count = 0;
+
+    public synchronized void increment(){
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
